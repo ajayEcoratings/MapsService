b9742289bf082a621af06f8712a4d2e152843f65
Ajay Mittal <ajay@ratings.eco>
2023-12-29 17:17:33 +0530

    nits

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
-const baseGooglePlacesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+const baseGooglePlacesURL =
+  "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 const filterNearbyPlaces = (data, validNames) => {
@@ -47,38 +47,57 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
 }
 
 export const findResults = async (lat, lng) => {
-    const keywords = ["river", "lake", "ocean", "sea"];
-    const circles = ["30000", "100000", "250000"];
-  
-    for (let radius of circles) {
-      for (let keyword of keywords) {
-        try {
-          const places = await makeRequest(keyword, radius, lat, lng);
-          const validPlaces = filterNearbyPlaces(places, keywords);
-  
-          if (validPlaces.length > 0) {
-            validPlaces.sort((A, B) => {
-              const distanceA = calculateDistance(lat, lng, A.geometry.location.lat, A.geometry.location.lng);
-              const distanceB = calculateDistance(lat, lng, B.geometry.location.lat, B.geometry.location.lng);
-              return distanceA - distanceB;
-            });
-  
-            return {
-              radiusLimit: radius,
-              nearestPlace: {
-                name: validPlaces[0].name,
-                lat: validPlaces[0].geometry.location.lat,
-                lng: validPlaces[0].geometry.location.lng,
-              },
-              distance: calculateDistance(lat, lng, validPlaces[0].geometry.location.lat, validPlaces[0].geometry.location.lng),
-            };
-          }
-        } catch (error) {
-          console.error("Error occurred:", error.message);
-          throw error;
+  const keywords = ["river", "lake", "ocean", "sea"];
+  const circles = ["30000", "100000", "250000"];
+
+  for (let radius of circles) {
+    for (let keyword of keywords) {
+      try {
+        const places = await makeRequest(keyword, radius, lat, lng);
+        const validPlaces = filterNearbyPlaces(places, keywords);
+
+        if (validPlaces.length > 0) {
+          validPlaces.sort((A, B) => {
+            const distanceA = calculateDistance(
+              lat,
+              lng,
+              A.geometry.location.lat,
+              A.geometry.location.lng
+            );
+            const distanceB = calculateDistance(
+              lat,
+              lng,
+              B.geometry.location.lat,
+              B.geometry.location.lng
+            );
+            return distanceA - distanceB;
+          });
+
+          return {
+            radiusLimit: radius/100,
+            nearestPlace: {
+              name: validPlaces[0].name,
+              lat: validPlaces[0].geometry.location.lat,
+              lng: validPlaces[0].geometry.location.lng,
+            },
+            distance: calculateDistance(
+              lat,
+              lng,
+              validPlaces[0].geometry.location.lat,
+              validPlaces[0].geometry.location.lng
+            ),
+          };
         }
+      } catch (error) {
+        console.error("Error occurred:", error.message);
+        throw error;
       }
     }
-  
-    return { radius: "251000" };
+  }
+
+  return {
+    radius: "250",
+    nearestPlace: null,
+    distance: null
   };
+};
